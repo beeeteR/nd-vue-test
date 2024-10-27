1445adf8c2b9993ffc44a75427628b62ba47e89d
beeeteR <130251761+beeeteR@users.noreply.github.com>
2024-10-27 19:20:22 +0400

    update vue.config

--- vue.config.ts
+++ vue.config.ts
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/nd-vue-test/'
     : '/'
-})
+}
